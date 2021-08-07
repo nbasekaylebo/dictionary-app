5381e39346875cbd42cf8df0e72a1fee4818b863
nbasekaylebo <nannalebo@gmail.com>
2021-08-07 09:43:22 +0100

    Added a search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Nastassja Basekay</small>
+        </footer>
+      </div>
     </div>
   );
 }
