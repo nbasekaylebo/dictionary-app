66c924f65056dea7062ff30741e2836476776883
nbasekaylebo <nannalebo@gmail.com>
2021-08-08 11:55:36 +0100

    Little bit of styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header text-center">
+          <h1>Dictionary</h1>
+        </header>
         <main>
           <Dictionary />
         </main>
